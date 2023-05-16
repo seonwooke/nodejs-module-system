93581a7cd02d73e88c9f5e4ef261656fbc9236b5
seonwooke <seonookkim@gmail.com>
2023-05-16 15:49:41 +0900

    :construction: Create Module
    Create Module

---
+++ https.js
@@ -0,0 +1,13 @@
+const request = require('./request');
+const response = require('./response');
+
+function makeRequest (url, data) {
+  // 요청을 보내기
+  request.send(url, data);
+
+  // 데이터를 return 하기
+  return response.read();
+}
+
+const responseData = makeRequest('https://naver.com', 'any daa');
+console.log(responseData);
