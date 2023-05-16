e1f153bc015102041ebc3c3e57e84fd29a6a76e9
seonwooke <seonookkim@gmail.com>
2023-05-16 16:01:11 +0900

    :construction: Code Simplification
    Code Simplification

--- https.js
+++ https.js
@@ -1,12 +1,12 @@
-const request = require('./request');
-const response = require('./response');
+const { send } = require('./request');
+const { read } = require('./response');
 
 function makeRequest (url, data) {
   // 요청을 보내기
-  request.send(url, data);
+  send(url, data);
 
   // 데이터를 return 하기
-  return response.read();
+  return read();
 }
 
 const responseData = makeRequest('https://naver.com', 'any daa');
